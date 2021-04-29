b8e51681af02def1c13bca6a8e7c7905c1cbe37d
Katie Han <katie@normalvr.com>
2021-04-29 14:37:56 -0400

    more content and animation

--- src/Interactive.js
+++ src/Interactive.js
@@ -3,14 +3,34 @@ import React from 'react';
 const QuizImages = [
     {
         src: './images/transmission-slide11.png',
-        credible: true
+        credible: true,
     },
     {
-        src: './images/covid-drzhong.jpg',
+        src: './images/rasam.jpg',
+        credible: false
+    },
+    {
+        src: './images/lungs.gif',
         credible: false
     },
     {
         src: './images/covid-garlic.jpg',
+        credible: true
+    },
+    {
+        src: './images/survival.png',
+        credible: false
+    },
+    {
+        src: './images/wuhan-garlic.jpg',
+        credible: false
+    },
+    {
+        src: './images/lemon.png',
+        credible: false
+    },
+    {
+        src: './images/fatality-rate.png',
         credible: true
     }
 ];
@@ -21,32 +41,42 @@ class Interactive extends React.Component {
         this.state = {
             currentIndex: 0,
             totalGuesses: 0,
-            correctGuesses: 0
+            correctGuesses: 0,
+            guessed: false
         };
     }
 
     guessOnClick(credible) {
+        this.setState({ guessed: true });
+
         let correctGuesses = this.state.correctGuesses;
         if (QuizImages[this.state.currentIndex] && QuizImages[this.state.currentIndex].credible == credible) {
             correctGuesses++;
         }
-        this.setState({
-            correctGuesses: correctGuesses,
-            totalGuesses: this.state.totalGuesses + 1,
-            currentIndex: this.state.currentIndex + 1
-        });
+
+        setTimeout(() => {
+            this.setState({
+                correctGuesses: correctGuesses,
+                totalGuesses: this.state.totalGuesses + 1,
+                currentIndex: this.state.currentIndex + 1,
+                guessed: false
+            });
+        }, 3000);
     }
 
     renderImage() {
         return ( this.state.currentIndex < QuizImages.length ?
-            <>
-                <img className='guess-image' src={QuizImages[this.state.currentIndex].src}></img>
-                <div className='buttons'>
+            <div className='image-wrapper'>
+                <div className={`guess-image ${this.state.guessed ? 'flip-image' : ''}`}>
+                    <img src={QuizImages[this.state.currentIndex].src}></img>
+                    <div className='image-back'>{QuizImages[this.state.currentIndex].credible ? 'CREDIBLE!' : 'NOT CREDIBLE!'}</div>
+                </div>
+                <div className={`buttons ${this.state.guessed ? 'hide-buttons' : ''}`}>
                     <div className='guess-button credible' onClick={this.guessOnClick.bind(this, true)}>CREDIBLE</div>
                     <div className='guess-button not-credible' onClick={this.guessOnClick.bind(this, false)}>NOT CREDIBLE</div>
                 </div>
-            </> :
-            <div>That's all we have!</div>
+            </div> :
+            <div className='image-wrapper'>That's all we have!</div>
         );
     }
 
