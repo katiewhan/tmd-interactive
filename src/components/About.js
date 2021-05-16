d59ff918be08276fc6303c9e2bed5a29145f97f6
Katie Han <katiehan95@gmail.com>
2021-05-16 16:30:11 -0400

    link to design proto

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,5 @@
+import '../styles/About.css';
+
 function About() {
   return (
     <div className='about'>
@@ -6,7 +8,7 @@ function About() {
         <div className='about-text'>While WhatsApp and other private messaging platforms have been powerful in enabling people to maintain relationships overseas and shaping mutual aid efforts prior to and during the COVID-19 crisis, they have also been misused to promote scams, conspiracy theories, digital surveillance, mob violence, and other types of abuse that sow confusion and chaos in the real world.</div>
         <div className='about-text'>The problem of misinformation spread on WhatsApp only accelerated with the pandemic, and it is difficult for researchers and organizations to keep track of inaccurate content circulating online, back-and-forth in multiple languages across countries with varying rates of digital literacy.</div>
         <div className='about-text'>We hope the activities, tips, and resources provided here empower users to detect false or misleading content on WhatsApp within their personal networks, and to take easy steps to ensure that the information they exchange with friends, family, or strangers is accurate and trustworthy — ultimately contributing to a healthier public sphere in the long run.</div>
-        <div className='about-text'><i>The website and survey are part of a research project by graduate students at NYU's Interactive Telecommunications Program for Tech, Media, and Democracy. Team members include Katie Han, Aditya Jain, Ami Mehta, and Shira Seri Levi.</i></div>
+        <div className='about-text'><i>The website and survey are part of a research project by graduate students at NYU's Interactive Telecommunications Program for Tech, Media, and Democracy. Team members include Katie Han, Aditya Jain, Ami Mehta, and Shira Seri Levi. <a className='about-link' href='https://www.youtube.com/watch?v=fo6wmocSSW0' target='_blank'>Check out the original design prototype for this website here.</a></i></div>
     </div>
   );
 }
